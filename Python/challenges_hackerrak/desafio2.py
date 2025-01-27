70e117d7139543982f8fa8f576923effc2d67187
LuisBarrionuevoDev <luisbarrionuevo.dev@gmail.com>
2025-01-27 02:41:13 -0300

    Se agregaron los challenges 1,2,3,4,5

---
+++ Python/challenges_hackerrak/desafio2.py
@@ -0,0 +1,33 @@
+print("Desafio 2") 
+''' Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+Example
+
+
+Print the following:
+
+8
+-2
+15
+Input Format
+
+The first line contains the first integer, .
+The second line contains the second integer, .
+
+Constraints'''
+a = int(input())
+b = int(input())
+    
+    
+def challenge(a,b):
+        print(a+b)
+        print(a-b)
+        print(a*b)
+   
+   
+challenge(a,b) 
+
