d99020390af9ead36bf5dffb430130ca44d9b006
LuisBarrionuevoDev <luisbarrionuevo.dev@gmail.com>
2025-01-27 11:45:14 -0300

    ss

--- Python/ejerciciosPracticos/analizarTexto.py
+++ Python/ejerciciosPracticos/analizarTexto.py
@@ -1,6 +1,12 @@
 import os
 print("Directorio actual:", os.getcwd())
-
+frase=[]
 with open("cuento1.txt", "r") as file1:
     for line in file1:
-        print(line)
+       frase += line.split(",")
+       
+           
+
+for words in frase:
+    print(words)
+    print(type(words))
