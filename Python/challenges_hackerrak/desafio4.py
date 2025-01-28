f677c44a15747bfbfb01380a5b9a7cf034fe0ec0
LuisBarrionuevoDev <luisbarrionuevo.dev@gmail.com>
2025-01-28 13:10:16 -0300

    se inclyo jupyter

--- Python/challenges_hackerrak/desafio4.py
+++ Python/challenges_hackerrak/desafio4.py
@@ -1,1 +1,11 @@
 print("Desafio 4")  
+'''Task
+The provided code stub reads an integer, , from STDIN. For all non-negative integers i<n , print i^2 .
+ '''
+n = int(input())
+    
+def challenge(n):
+    for i in range(0,n):
+        print(i*i)
+
+challenge(n)   
