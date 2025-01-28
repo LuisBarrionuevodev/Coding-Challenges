d50af1096fad185f99248ef4e298d8125f5a303d
LuisBarrionuevoDev <luisbarrionuevo.dev@gmail.com>
2025-01-28 13:10:16 -0300

    se inclyo jupyter

--- Python/challenges_hackerrak/desafio3.py
+++ Python/challenges_hackerrak/desafio3.py
@@ -1,1 +1,12 @@
 print("Desafio 3") 
+'''Task
+The provided code stub reads two integers,  and , from STDIN.
+
+Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / . '''
+a = int(input())
+b = int(input())
+    
+def challenge(a,b):
+        print(a//b)
+        print(a/b)
+challenge(a,b)  
---
+++ Python/challenges_hackerrak/desafio6.py
@@ -0,0 +1,11 @@
+n= int(input("Ingresa un numero"))
+
+def Cuenta(n):
+    numero=[]
+    for i in range(1,n+1):
+        numero.append(i)
+
+    return print(numero)    
+
+
+Cuenta(n)
